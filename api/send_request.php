4058671afb6bebd4d242dc7a1f721d8054ad8f45
aryanksh <aryanksh@gmail.com>
2023-09-02 21:46:52 -0400

    beginning to send data to database

---
+++ api/send_request.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', '1');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// set_include_path("./includes/");
+require_once("./includes/PHPMailer/src/Exception.php");
+require_once("./includes/PHPMailer/src/PHPMailer.php");
+
+
+
+// require 'path/to/PHPMailer/src/Exception.php';
+// require 'path/to/PHPMailer/src/PHPMailer.php';
+
+
+?>
