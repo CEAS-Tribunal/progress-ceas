bbe3eccf535a8b4eb5696dab0dd2e75ba6f03d86
aryanksh <aryanksh@gmail.com>
2023-09-18 22:14:14 -0400

    emails are sent to academic affairs chairs

--- api/send_request.php
+++ api/send_request.php
@@ -48,15 +48,14 @@ $coop_resources = mysqli_real_escape_string($mysqli, strip_tags(trim($_POST["coo
 $student_led = mysqli_real_escape_string($mysqli, strip_tags(trim($_POST["studentLed"])));
 $follow_up = mysqli_real_escape_string($mysqli, strip_tags(trim($_POST["followUp"])));
 
-if (!isset($result_data))
-{
+if (!isset($result_data)) {
     $result_data = new stdClass();
 }
 $result_data->status = "error";
 $result_data->message = "";
 
 // regex form input validation
-if ($disclose_contact == "Yes"){
+if ($disclose_contact == "Yes") {
     // check name
     if (!($name == "") && !preg_match("/^[\w\ \'\.\/]{1,128}$/", $name)) {
         $result_data->message = 'Your name is invalid. Please only use latin characters a-z with an optional '
@@ -67,8 +66,7 @@ if ($disclose_contact == "Yes"){
 
     // check major
     if (!($major == "") && !preg_match("/^[\w\ \'\.\/]{1,128}$/", $major)) {
-        $result_data->message = 'Your major is invalid. Please only use latin characters a-z with an optional '
-            . 'apostrophe or period. Your major is also limited to 128 characters.';
+        $result_data->message = 'Your major is invalid. Please only use latin characters a-z. Your major is also limited to 128 characters.';
         echo json_encode($result_data);
         die();
     }
@@ -81,61 +79,60 @@ if ($disclose_contact == "Yes"){
     }
 
     // Check email address
-    if(!($email == "") && !preg_match('/^[\w\W]+@[\w\W\d\/]{1,254}$/', $email)) {
-        $result_data->message = 'Your email address is invalid. Please use an email in the following format: <>@<>. '
+    if (!($email == "") && !preg_match('/^[\w\W]+@[\w\W\d\/]{1,254}$/', $email)) {
+        $result_data->message = 'Your email address is invalid. Please use an email in the following format: <6+2>@<mail.uc.edu>. '
             . 'Your email is also limited to 254 characters.';
-            $result_data->message = 'this is your email: ' . $email;
+        // $result_data->message = 'this is your email: ' . $email;
         echo json_encode($result_data);
         die();
     }
+}
+// check concern
+if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $concern)) {
+    $result_data->message = 'Your concern(s) are invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
+        . 'apostrophe, or period. Your response is also limited to 500 characters.';
+    echo json_encode($result_data);
+    die();
+}
 
-    // check concern
-    if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $concern)) { 
-        $result_data->message = 'Your concern(s) are invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
-            . 'apostrophe, or period. Your response is also limited to 500 characters.';
-        echo json_encode($result_data);
-        die();
-    }
+// check academic comments
+if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $academic_comment)) {
+    $result_data->message = 'Your comments on academics is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
+        . 'apostrophe, or period. Your response is also limited to 500 characters.';
+    echo json_encode($result_data);
+    die();
+}
 
-    // check academic comments
-    if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $academic_comment)) { 
-        $result_data->message = 'Your comments on academics is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
-            . 'apostrophe, or period. Your response is also limited to 500 characters.';
-        echo json_encode($result_data);
-        die();
-    }
+// check coop comments
+if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $coop_comment)) {
+    $result_data->message = 'Your comments on co-ops is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
+        . 'apostrophe, or period. Your response is also limited to 500 characters.';
+    echo json_encode($result_data);
+    die();
+}
 
-    // check coop comments
-    if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $coop_comment)) { 
-        $result_data->message = 'Your comments on co-ops is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
-            . 'apostrophe, or period. Your response is also limited to 500 characters.';
-        echo json_encode($result_data);
-        die();
-    }
+// check specific changes
+if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $specific_changes)) {
+    $result_data->message = 'Your response regarding specific changes is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
+        . 'apostrophe, or period. Your response is also limited to 500 characters.';
+    echo json_encode($result_data);
+    die();
+}
 
-    // check specific changes
-    if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $specific_changes)) { 
-        $result_data->message = 'Your response regarding specific changes is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
-            . 'apostrophe, or period. Your response is also limited to 500 characters.';
-        echo json_encode($result_data);
-        die();
-    }
+// check academic resources response
+if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $academic_resources)) {
+    $result_data->message = 'Your response regarding academic resources is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
+        . 'apostrophe, or period. Your response is also limited to 500 characters.';
+    echo json_encode($result_data);
+    die();
+}
 
-    // check academic resources response
-    if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $academic_resources)) { 
-        $result_data->message = 'Your response regarding academic resources is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
-            . 'apostrophe, or period. Your response is also limited to 500 characters.';
-        echo json_encode($result_data);
-        die();
-    }
-
-    // check co-op resources response
-    if (!preg_match("/^[A-Za-z0-9\.\!\@\#\$\%\^\& \*\(\)\-\_\+\=\`\~\,\.\?\/\<\>\;\:\.]{1,500}$/", $coop_resources)) { 
-        $result_data->message = 'Your response regarding co-op resources is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>, \',.'
-            . 'apostrophe, or period. Your response is also limited to 500 characters.';
-        echo json_encode($result_data);
-        die();
-    }
+// check co-op resources response
+if (!preg_match("/^[A-Za-z0-9\.\'\!\@\#\$\%\^\& \*\(\)\-\_\+\`\=\~\,\.\?\/\<\>\;\:]{1,500}$/", $coop_resources)) {
+    $result_data->message = 'Your response regarding co-op resources is invalid. Please only use latin characters a-z with an optional \'!@#$%^&*()_-+=:;?.`~<>\', '
+        . 'comma, apostrophe, or period. Your response is also limited to 500 characters.';
+    echo json_encode($result_data);
+    die();
 }
 
 // get admin data
@@ -146,7 +143,7 @@ $chair_2_email = '';
 
 $super_email = '';
 
-$sql  ='SELECT `chair_1_name`, `chair_1_email`, `chair_2_name`, `chair_2_email`, `super_email` FROM `progress_ceas_admin`';
+$sql  = 'SELECT `chair_1_name`, `chair_1_email`, `chair_2_name`, `chair_2_email`, `super_email` FROM `progress_ceas_admin`';
 $result = $mysqli->query($sql);
 
 if ($result) {
@@ -162,7 +159,7 @@ if ($result) {
 
 if ($chair_1_name === "" || $chair_1_email === "" || $chair_2_name === "" || $chair_2_email === "" || $super_email === "") {
     $result_data->message = "Error occurred while retrieving admin information. Please try again. "
-    . "If the error persists, email the admin in the description.";
+        . "If the error persists, email the admin in the description.";
     echo json_encode($result_data);
     die();
 }
@@ -175,28 +172,71 @@ $currentDate = $currentDateTime->format('l, F j, Y h:i:sa');
 // insert form data into database
 $sql = "INSERT INTO `progress_ceas_main` (`date`, `disclose_contact`, `name`, `major`, `grad_year`, `email`, `concern`,
 `academic_satisfied_level`, `coop_satisfied_level`, `academic_comment`, `coop_comment`, `specific_changes`,
-`academic_resources`, `coop_resources`, `student_led`, `follow_up`) VALUES ('".$currentDate."', '".$disclose_contact."', '".$name."', '".$major."', '".$grad_year."', '".$email."',
-'".$concern."', '".$academic_satisfied_level."', '".$coop_satisfied_level."', '".$academic_comment."', '".$coop_comment."', 
-'".$specific_changes."', '".$academic_resources."', '".$coop_resources."', '".$student_led."', '".$follow_up."')";
-
-// REIMBURSEMENT VERSION
-// $sql = 'INSERT INTO `reimbursement_main` (`name`, `position`, `email`, `mid`, `date`, `vendor`, `amount`, `description`, `status`,`type`,
-// `receipt_name`,`document_name`,`officer_name`,`officer_position`,`address`)'
-// . "VALUES ('".$name."','".$position."','".$email."','".$m_id."','".$date."','".$vendor."','".$amount."','".$description."','".$budgeted."',
-// '".$direct."','".$receiptText."','".$docsText."','".$officer_name."','".$officer_position."','".$address."')";
+`academic_resources`, `coop_resources`, `student_led`, `follow_up`) VALUES ('" . $currentDate . "', '" . $disclose_contact . "', '" . $name . "', '" . $major . "', '" . $grad_year . "', '" . $email . "',
+'" . $concern . "', '" . $academic_satisfied_level . "', '" . $coop_satisfied_level . "', '" . $academic_comment . "', '" . $coop_comment . "', 
+'" . $specific_changes . "', '" . $academic_resources . "', '" . $coop_resources . "', '" . $student_led . "', '" . $follow_up . "')";
 
 $result = $mysqli->query($sql);
 
 if (!$result) {
     $result_data->message = 'Error occurred while submitting your reimbursement request. Please try again. '
-    . 'If the error persists, email the admin in the description.';
+        . 'If the error persists, email the admin in the description.';
     echo json_encode($result_data);
     die();
 }
 
-$result_data->message = $chair_1_email;
-echo json_encode($result_data);
-die();
+$mail = new PHPMailer(true);
 
-// TODO: Write emails next
-?>
+// Emailing Academic Affairs chairs
+//TODO: Change font style and size
+try {
+    $mail->IsHTML(true);
+
+    $mail->Subject = "Progress CEAS Response Received";
+
+    $email_msg = "Hello " . $chair_1_name . " and " . $chair_2_name . ",<br /> <br />";
+    $email_msg .= "Someone has filled out the Progress CEAS form on " . $currentDate . "!<br /> <br />";
+
+    if ($disclose_contact == "Yes") {
+        $email_msg .= "Name: " . $name . " <br />";
+        $email_msg .= "Major: " . $major . " <br />";
+        $email_msg .= "Graduation Year: " . $grad_year . " <br />";
+        $email_msg .= "UC Email: " . $email . " <br /> <br />";
+    } else {
+        $email_msg .= "<b>The user did not want to disclose their contact information.</b> <br /> <br />";
+    }
+    $email_msg .= "What concern brought you to this form?<br />" . $concern . " <br /> <br />";
+    $email_msg .= "How satisfied are you with your academic environment at CEAS?<br />" . $academic_satisfied_level . " <br /> <br />";
+    $email_msg .= "How satisfied are you with your co-op experience at CEAS?<br />" . $coop_satisfied_level . " <br /> <br />";
+    $email_msg .= "What comments on academics do you have?<br />" . $academic_comment . " <br /> <br />";
+    $email_msg .= "What comments on co-op do you have?<br />" . $coop_comment . " <br /> <br />";
+    $email_msg .= "What specific changes do you want to see based on your previous responses?<br />" . $specific_changes . " <br /> <br />";
+    $email_msg .= "What resources have been helpful to you in navigating your academic life?<br />" . $academic_resources . " <br /> <br />";
+    $email_msg .= "What resources have been helpful in navigating your co-op searches?<br />" . $coop_resources . " <br /> <br />";
+
+    if ($disclose_contact == "Yes") {
+        $email_msg .= "Would you be willing to talk to the student-led Academic Affairs Committee about your comments?<br />" . $student_led . " <br /> <br />";
+        $email_msg .= "Would you like us to follow up with you on any steps we take?<br />" . $follow_up . " <br /> <br />";
+    }
+
+    $email_msg .= "<b>Please reach out to one of the tech chairs through Slack or just reply to this email if you have any questions.</b> <br /> <br />";
+
+    $email_msg .= "Best regards,<br />";
+    $email_msg .= $super_email;
+
+    $mail->Body = $email_msg;
+    $mail->setFrom($super_email);
+    $mail->addAddress($chair_1_email, $chair_1_name); //TODO: replace with academic affairs chairs' info in phpmyadmin
+    //$mail->addAddress($chair_2_email, $chair_2_name);
+
+    $mail->send();
+} catch (Exception $e) {
+    $result_data->message = 'Error occurred while sending the admin the confirmation email. Please email the admin in the description notifying of this error.';
+    echo json_encode($result_data);
+    die();
+}
+
+$result_data->status = "success";
+echo json_encode($result_data);
+
+mysqli_close($mysqli);
